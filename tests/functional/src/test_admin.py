db184a4814eaa6bd18ac6f6d3fd805a1f4cfe0a4
Tatyana Akimova <takimova@ivi.ru>
2023-08-04 21:16:12 +0300

    add test fiction

--- tests/functional/src/test_admin.py
+++ tests/functional/src/test_admin.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 import pytest
 
+from tests.functional.testdata.admin import get_notification_data
 from tests.functional.utils.routes import ADMIN_URL
 
 pytestmark = pytest.mark.asyncio
@@ -19,3 +20,33 @@ class TestFilm:
         for _ in response.body['events']:
             for field in expected_fields:
                 assert field in response.body, f'No {field} in response'
+
+    async def test_get_notification(self, make_get_request, make_post_request):
+        data = get_notification_data()
+        await make_post_request(ADMIN_URL, data=data)
+
+        response = await make_get_request(f'{ADMIN_URL}/{data["id"]}')
+
+        assert response.status == HTTPStatus.OK, 'Wrong status code'
+
+    async def test_add_notification(self, make_post_request):
+        data = get_notification_data()
+        response = await make_post_request(ADMIN_URL, data=data)
+
+        assert response.status == HTTPStatus.CREATED, 'Wrong status code'
+        assert response.body['msg'] == 'Notification added', 'Wrong message'
+
+    async def test_update_notification(self, make_put_request, make_post_request):
+        data = get_notification_data()
+        await make_post_request(ADMIN_URL, data=data)
+        response = await make_put_request(f'{ADMIN_URL}/{data["id"]}', data=data)
+
+        assert response.status == HTTPStatus.OK, 'Wrong status code'
+        assert response.body['msg'] == 'Notification updated', 'Wrong message'
+
+    async def test_delete_notification(self, make_delete_request, make_post_request):
+        data = get_notification_data()
+        await make_post_request(ADMIN_URL, data=data)
+        response = await make_delete_request(f'{ADMIN_URL}/{data["id"]}')
+
+        assert response.status == HTTPStatus.NO_CONTENT, 'Wrong status code'
--- tests/functional/testdata/admin.py
+++ tests/functional/testdata/admin.py
@@ -7,7 +7,7 @@ fake = Faker()
 
 async def get_notification_data() -> dict:
     data = {
-        # 'id': str(uuid.uuid4()),
+        'id': str(uuid.uuid4()),
         'description': fake.text(max_nb_chars=200),
         'is_unsubscribeable': True,
         'cron_string': '0 0 * * * *',
