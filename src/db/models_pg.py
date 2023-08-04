9b73f10e5abbac4ac5a4da5a978e92cf05f2752f
liana <liana.nigmatullina@tetrika.school>
2023-08-05 02:04:14 +0300

    review fixes

--- src/db/models_pg.py
+++ src/db/models_pg.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Boolean, Column, DateTime, Enum, ForeignKey, String
+from sqlalchemy import Boolean, Column, DateTime, Enum, ForeignKey, String, Text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -20,36 +20,41 @@ else:
 class Event(Base):
     __tablename__ = 'events'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    description = Column(String)
-    is_unsubscribeable = Column(Boolean)
+    description = Column(String(150))
+    is_unsubscribeable = Column(Boolean, nullable=False, default=False)
 
 
 class EventScheduled(Base):
     __tablename__ = 'events_scheduled'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    event_id = Column(UUID(as_uuid=True), ForeignKey('events.id'))
-    cron_string = Column(String)
+    event_id = Column(UUID(as_uuid=True), ForeignKey('events.id', ondelete='CASCADE'), nullable=False)
+    cron_string = Column(String(200), nullable=False)
 
 
 class ScheduledEventUser(Base):
     __tablename__ = 'scheduled_events_users'
-    scheduled_event_id = Column(UUID(as_uuid=True), ForeignKey('events_scheduled.id'), primary_key=True)
+    scheduled_event_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey('events_scheduled.id', ondelete='CASCADE'),
+        primary_key=True
+    )
     user_id = Column(UUID(as_uuid=True), primary_key=True)
 
 
 class Template(Base):
     __tablename__ = 'templates'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    title = Column(String)
-    description = Column(String)
-    template = Column(String)
+    description = Column(String(100))
+    template = Column(Text, nullable=False)
+    title = Column(String(50))
 
 
 class NotificationTemplate(Base):
     __tablename__ = 'notification_templates'
-    event_id = Column(UUID(as_uuid=True), ForeignKey('events.id'), primary_key=True)
-    channel: Channel = Column(Enum(Channel))
-    template_id = Column(UUID(as_uuid=True), ForeignKey('templates.id'))
+    event_id = Column(UUID(as_uuid=True), ForeignKey('events.id', ondelete='CASCADE'), primary_key=True)
+    channel: Channel = Column(Enum(Channel), nullable=False)
+    template_id = Column(UUID(as_uuid=True), ForeignKey('templates.id', ondelete='CASCADE'), nullable=False)
+
     event = relationship('Event')
     template = relationship('Template')
 
@@ -57,20 +62,20 @@ class NotificationTemplate(Base):
 class UserUnsubscribed(Base):
     __tablename__ = 'users_unsubscribed'
     user_id = Column(UUID(as_uuid=True), primary_key=True)
-    channel: Channel = Column(Enum(Channel))
+    channel: Channel = Column(Enum(Channel), nullable=False)
 
 
 class NotificationSent(Base):
     __tablename__ = 'notifications_sent'
-    user_id = Column(UUID(as_uuid=True), primary_key=True)
-    event_id = Column(UUID(as_uuid=True), ForeignKey('events.id'))
-    delivery_dt = Column(DateTime)
+    user_id = Column(UUID(as_uuid=True), nullable=False)
+    event_id = Column(UUID(as_uuid=True), ForeignKey('events.id', ondelete='CASCADE'), nullable=False)
+    delivery_dt = Column(DateTime, nullable=False)
 
 
 class ShortLinks(Base):
     __tablename__ = 'short_links'
-    short_link = Column(String, primary_key=True)
-    original_link = Column(String, default=auth_config.url_verify)
-    user_id = Column(UUID(as_uuid=True))
+    short_link = Column(String(20), primary_key=True)
+    original_link = Column(String(250), default=auth_config.url_verify, nullable=False)
+    user_id = Column(UUID(as_uuid=True), nullable=False)
     ttl = Column(DateTime, nullable=False, default=datetime.utcnow)
-    redirect_url = Column(String, default=auth_config.url_redirect)
+    redirect_url = Column(String(250), default=auth_config.url_redirect)
